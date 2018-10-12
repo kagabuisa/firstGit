695e1705b48a87c59c9e48ef656af2f1ea236c9b
Kagabu Isa <kagabuisa@gmail.com>
2018-10-12 23:54:42 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// const arr = ['cat', 'dog', 'fish'];
+// for (i = 0; i < arr.length; i++) { 
+//  alert(arr[i])
+// }
+
+// var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi", "Opel"];
+// var text = "";
+// var i;
+// for (i = 0; i < cars.length; i++) {
+//     text += "German " + cars[i] + " is cool car" + "<br>";
+    
+// }
+// alert(text);
+
+// let products = [
+//     { name: 'Running shoes', price: 75 },
+//     { name: 'Golf shoes',    price: 85 },
+//     { name: 'Dress shoes',   price: 95 },
+//     { name: 'Walking shoes', price: 65 },
+//     { name: 'Sandals',       price: 55 }
+//   ];
+
+//   for (let i = 0; i < products.length; i++) {
+//     console.log(products[i]);
+//   }
+
+//     var data = [1,2,3,4,5];
+//     var sumexe = 0;
+//     data.forEach(function(value) {
+//         sumexe += value; 
+//         alert(sumexe);
+//     });
+
+function myfunc() {
+    var s = 300
+    alert(s);
+    return;
+}   
+
+var myVar = 1000000000
+alert(myVar);
+
+var myVar = prompt("<Good morning Colleagues>")
+alert(myVar);
+
+var a = "100"
+var b = 25
+alert(a/b);
+
+var hullo = 1
+
