51ebf28801a344bc7089257608a9954ad8f02197
Kagabu Isa <kagabuisa@gmail.com>
2018-11-03 22:38:49 +0300

    Comitted changes

--- app.js
+++ app.js
@@ -1,65 +1,28 @@
-// const arr = ['cat', 'dog', 'fish'];
-// for (i = 0; i < arr.length; i++) { 
-//  alert(arr[i])
-// }
+var person = {
+    firstname: "Kagabu",
+    lastname: "Isa",
+    getFullName: function(){
+    var fullname = this.firstname + " " + this.lastname;
+    return fullname;
+    }
 
-// var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi", "Opel"];
-// var text = "";
-// var i;
-// for (i = 0; i < cars.length; i++) {
-//     text += "German " + cars[i] + " is cool car" + "<br>";
-    
-// }
-// alert(text);
+}
 
-// let products = [
-//     { name: 'Running shoes', price: 75 },
-//     { name: 'Golf shoes',    price: 85 },
-//     { name: 'Dress shoes',   price: 95 },
-//     { name: 'Walking shoes', price: 65 },
-//     { name: 'Sandals',       price: 55 }
-//   ];
+var logName = function(lang1,lang2){
+    console.log('Logged '+ this.getFullName());
+    console.log('Arguments ' + lang1 + " " + lang2)
+    console.log("----------------")
+}
 
-//   for (let i = 0; i < products.length; i++) {
-//     console.log(products[i]);
-//   }
+var logPersonName = logName.bind(person);
 
-  // alert(products[1]);
-//     var data = [1,2,3,4,5];
-//     var sumexe = 0;
-//     data.forEach(function(value) {
-//         sumexe += value; 
-//         alert(sumexe);
-//     });
+logPersonName();
 
-// function myfunc() {
-//     var s = 300
-//     alert(s);
-//     return;
-// }   
+logName.call(person,"en","es");
+logName.apply(person,["en","es"]);
 
-// var myVar = 1000000000
-// alert(myVar);
-
-// var myVar = prompt("<Good morning Colleagues>")
-// alert(myVar);
-
-var a = "100"
-var b = 25
-alert(a/b);
-
-var hullo = 1
-alert(hullo);
-
-function gite(){
-  var gitApproach = "Add Commit Push"
-  var noise = gitApproach + " "+ "Pull";
-  return noise;
-  }
-
-var isma = gite();
-document.write(isma + " "+"to get ANSWER");
-
-var kagabu = ("  My phone number is " + prompt('enter phone no.'))
-document.write(kagabu);
-console.log(kagabu);
+(function(lang1,lang2){
+  console.log('Logged '+ this.getFullName());
+  console.log('Arguments ' + lang1 + " " + lang2)
+  console.log("----------------")
+}).apply(person,['en','es']);
